9fc2774e732cdc9f0b03a6ebafd23ca588fd9512
Serhii <opalinskyiserhii@gmail.com>
2024-05-01 17:38:16 +0300

    add absolute path

--- src/pageObjects/WelcomePage/WelcomePage.js
+++ src/pageObjects/WelcomePage/WelcomePage.js
@@ -1,5 +1,5 @@
-import {SignUpPopup} from "./components/SignUpPopup";
-import {SignInPopup} from "./components/SignInPopup.js";
+import {SignUpPopup} from "/src/pageObjects/WelcomePage/components/SignUpPopup";
+import {SignInPopup} from "/src/pageObjects/WelcomePage/components/SignInPopUp.js";
 import BasePage from "../BasePage.js";
 import GaragePage from "../GaragePage/GaragePage.js";
 
